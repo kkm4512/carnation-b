261e8c24275175c08c03945531aac7c1ad763f61
km340 <nayoun340@gmail.com>
2025-03-09 15:01:15 +0900

    init

---
+++ src/main/java/com/example/carnation/CarnationApplication.java
@@ -0,0 +1,13 @@
+package com.example.carnation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarnationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carnation/CarnationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carnation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
