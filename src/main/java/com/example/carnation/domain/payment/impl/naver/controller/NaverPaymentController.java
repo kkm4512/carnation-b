f4ca8c18158d7bc07c60556d885b64ff59edd88c
Terror <nayoun440@naver.com>
2025-03-26 14:41:03 +0900

    [#110] test. AI 코드 리뷰 테스트

--- src/main/java/com/example/carnation/domain/payment/impl/naver/controller/NaverPaymentController.java
+++ src/main/java/com/example/carnation/domain/payment/impl/naver/controller/NaverPaymentController.java
@@ -3,6 +3,7 @@ package com.example.carnation.domain.payment.impl.naver.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,10 @@ public class NaverPaymentController {
     public String perpare() {
         return "perpare1111";
     }
+
+    @GetMapping("/{id}")
+    public String perpare(@PathVariable String id) {
+        return "perpare2222";
+    }
+
 }
